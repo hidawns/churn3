c677dd415a54ed20bd760def4741aef7e45c99f9
hidawns <134594505+hidawns@users.noreply.github.com>
2025-08-11 03:38:56 +0700

    Update Home.py

--- modules/Home.py
+++ modules/Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def show():
     st.title("Churn Prediction App")
-    st.markdown("##### Optimalisasi Model XGBoost Menggunakan Teknik Hybrid Resampling SMOTE-ENN<br>dan Hyperparameter Tuning pada Pipeline Prediksi Churn Pelanggan", unsafe_allow_html=True)
+    st.markdown("##### Optimalisasi Model XGBoost Menggunakan Teknik Hybrid Resampling SMOTE-ENN<br>dan Hyperparameter Tuning GridSearchCV dalam Prediksi Churn Pelanggan", unsafe_allow_html=True)
     st.write("Dikembangkan oleh: Hidayati Tri Winasis")
 
     st.markdown("---")  
@@ -17,15 +17,17 @@ def show():
             line-height: 1.4;
             text-align: justify'>
         Aplikasi ini bertujuan untuk memprediksi kemungkinan seorang pelanggan melakukan churn. Aplikasi dibangun dengan mengimplementasikan machine learning melalui algoritma <b>XGBoost</b> 
-        yang dioptimalkan dengan teknik resampling <b>SMOTE-ENN</b> serta preprocessing yang disesuaikan untuk dataset terbaru.
+        yang dioptimalkan dengan teknik resampling <b>SMOTE-ENN</b> serta hyperparameter tuning <b>GridSearchCV</b>.
         </div>
         """, unsafe_allow_html=True)
 
+        
         st.markdown(" ")
         if st.button("**Pergi ke Halaman Prediksi**"):
             st.session_state.page = "Inference"
             st.rerun()
         
+            
     with col2:
         st.image("assets/churn_image4.jpg", use_container_width=True)
         
