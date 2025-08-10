0fba6ab1f1477a28b92af141e9298e94edc7284f
hidawns <134594505+hidawns@users.noreply.github.com>
2025-08-11 03:41:00 +0700

    Update Overview.py

--- modules/Overview.py
+++ modules/Overview.py
@@ -3,12 +3,15 @@ import streamlit as st
 def show():
     st.title("Mengenal Customer Churn")
 
+    # SECTION 1 - Penjelasan Churn
+    #st.markdown("\n\n")
     st.markdown("### Apa Itu Customer Churn?")
     st.markdown("""
 - Customer churn adalah kondisi dimana seorang pelanggan berhenti menggunakan layanan dari suatu perusahaan, dan beralih menggunakan layanan dari perusahaan lain.
 - Churn merupakan tantangan utama yang kerap dihadapi oleh industri telekomunikasi dengan kondisi pasar yang dipenuhi persaingan antar penyedia layanan.
 """)
 
+    # SECTION 2 - Penyebab churn
     st.markdown("---")
     st.markdown("### Penyebab Churn Pelanggan")
     
@@ -25,15 +28,17 @@ def show():
 
     st.markdown("---")
 
+    # SECTION 2 - Dampak Churn
     st.markdown("### Dampak Churn Pelanggan")
     st.markdown("""
 - Tingginya tingkat churn pelanggan dapat menyebabkan penurunan pendapatan perusahaan secara signifikan.
 - Perusahaan juga harus mengeluarkan biaya yang lebih besar untuk dapat mengakuisisi pelanggan baru.
-- Upaya mengakuisisi pelanggan baru umumnya memakan biaya yang jauh lebih besar dibandingkan mempertahankan pelanggan yang sudah ada.
+- Upaya mengakuisisi pelanggan baru umumnya memakan biaya yang jauh lebih besar, yakni 5-10 kali lipat lebih mahal dibandingkan mempertahankan pelanggan yang sudah ada.
 """)
 
     st.markdown("---")
 
+    # SECTION 4 - Manfaat Prediksi Churn
     st.markdown("### Manfaat prediksi churn")
     st.info("""
 - Prediksi churn membantu perusahaan mengidentifikasi secara dini pelanggan yang berisiko berhenti menggunakan produk atau layanan. 
