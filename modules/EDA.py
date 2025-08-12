d4efb00e86dada94fcf25f16d10481239ea94afe
hidawns <134594505+hidawns@users.noreply.github.com>
2025-08-12 17:54:47 +0700

    Update EDA.py

--- modules/EDA.py
+++ modules/EDA.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import math
 
 def show():
     sns.set_theme(style="whitegrid", palette="pastel")
