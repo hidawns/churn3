42a615b222edc3d4f52bf4843ec44e79dc3b193b
hidawns <134594505+hidawns@users.noreply.github.com>
2025-08-12 18:08:51 +0700

    Update EDA.py

--- modules/EDA.py
+++ modules/EDA.py
@@ -6,7 +6,7 @@ import math
  
 def show():
     sns.set_theme(style="whitegrid", palette="pastel")
-    st.title("📈 Exploratory Data Analysis (EDA)")
+    st.title("📈 Exploratory Data Analysis (EDA) cek")
 
     df = pd.read_csv("merged_dataset.csv")
 
